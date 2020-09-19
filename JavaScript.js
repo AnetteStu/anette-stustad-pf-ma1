9c16a2c5a8e6dd0f1af7c8bbe1f146aeb8aae100
AnetteStu <69764146+AnetteStu@users.noreply.github.com>
2020-09-19 22:12:33 +0200

    Create JavaScript.js

---
+++ JavaScript.js
@@ -0,0 +1,29 @@
+// Question 1
+var firstVar; 
+// Question 2
+var name = "Anette";
+// Question 3
+var elleven = 11;
+// Question 4
+var division = 20/5;
+// Question 5
+typeof ("frog");
+// Question 6
+var orderHasShipped = true;
+
+// Question 7
+if (orderHasShipped === true) {
+    console.log(true);
+}
+else {
+    console.log(false)
+}
+// Question 8
+var fruits = [ "Banana", "Apple", "Pear", "Watermelon", "Orange", "Cherry", "Coconut", "Plum", "Lime", "Lemon" ];
+
+var numberOfFruits = fruits.length;
+console.log(numberOfFruits)
+
+for (var i = 0; i < numberOfFruits; i++) {
+    console.log(fruits[i]);
+}
